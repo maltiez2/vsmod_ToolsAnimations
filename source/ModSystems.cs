0f4b5badbc43747fc9befbe8ffa38fab7db5346d
maltiez2 <maltiez2@gmail.com>
2025-04-08 08:08:45 +0300

    Add project files.

---
+++ source/ModSystems.cs
@@ -0,0 +1,31 @@
+﻿using CombatOverhaul.Armor;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Server;
+using Vintagestory.Client.NoObf;
+using Vintagestory.Server;
+
+namespace ToolsAnimations;
+
+public sealed class ToolsAnimationsSystem : ModSystem
+{
+    public BlockBreakingSystemClient? ClientBlockBreakingSystem { get; private set; }
+    public BlockBreakingSystemServer? ServerBlockBreakingSystem { get; private set; }
+
+    public override void Start(ICoreAPI api)
+    {
+        api.RegisterItemClass("ToolsAnimations:Axe", typeof(Axe));
+        api.RegisterItemClass("ToolsAnimations:Pickaxe", typeof(Pickaxe));
+    }
+
+    public override void StartClientSide(ICoreClientAPI api)
+    {
+        ClientBlockBreakingSystem = new(api);
+    }
+
+    public override void StartServerSide(ICoreServerAPI api)
+    {
+        ServerBlockBreakingSystem = new(api);
+    }
+}
